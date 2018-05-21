cbad3df06d149259a4aa283a99759264418da009
schwehn42 <schwehn42@gmail.com>
2018-05-21 14:08:30 +0200

    Projektdateien hinzufügen.

---
+++ ArmaComparison/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArmaComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lineStrings = System.IO.File.ReadAllLines(@"C:\Users\schwe\Desktop\testReadFile.txt");
+            foreach (string lineString in lineStrings)
+            {
+                Console.WriteLine(lineString);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
