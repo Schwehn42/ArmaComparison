f87fe7b33f3a3bad8a88bd4341f19049412df903
schwehn42 <schwehn42@gmail.com>
2018-05-21 14:56:00 +0200

    renamed file from Program to ArmaModFile first step to make it OOP; constructor and object vars

---
+++ ArmaComparison/ArmaModFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace ArmaComparison
+{
+    class ArmaModFile
+    {
+
+        private string path { get; }
+        private List<string> idList { get; }
+
+        public ArmaModFile(string path)
+        {
+            this.path = path;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string pathFile1 = @"C:\Users\schwe\Desktop\Arma 3 Mod Preset ww2.html";
+            var doc = new HtmlDocument();
+            doc.Load(pathFile1);
+
+            var nodes = doc.DocumentNode.SelectNodes("//body//table//a");
+
+            foreach (HtmlNode node in nodes)
+            {
+
+                Console.WriteLine(node.InnerText.Split("id=")[1]);
+                
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
